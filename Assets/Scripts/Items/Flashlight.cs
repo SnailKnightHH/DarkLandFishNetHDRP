048bdcc168b547172023007c89964b4a7eb9a4b5
SnailKnightHH <huyangming2016@gmail.com>
2023-12-12 21:34:44 -0500

    fixed flash light toggle not network synced bug

--- Assets/Scripts/Items/Flashlight.cs
+++ Assets/Scripts/Items/Flashlight.cs
@@ -1,3 +1,4 @@
+using FishNet.Object;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -13,10 +14,32 @@ public class Flashlight : Tool
         light.enabled = false;
     }
 
+    [ServerRpc(RequireOwnership = false, RunLocally = true)]
+    private void ToggleFlashLightServerRpc()
+    {
+        light.enabled = !light.enabled;
+        ToggleFlashLightClientRpc();
+    }
+
+    // ExcludeOwner -> client calls server rpc, which runs locally, then other clients receive update. Exclude owner so action initiator does not execute action twice.
+    // ExcludeServer -> client calls server rpc, if server is a client itself (ie. it's a host), then server rpc run locally (count = 1), then client rpc runs on host (count = 2) -> execute action twice
+    [ObserversRpc(ExcludeOwner = true, ExcludeServer = true)]
+    private void ToggleFlashLightClientRpc()
+    {
+        light.enabled = !light.enabled;
+    }
 
     public override IEnumerator UseTool(Player player)
     {
-        light.enabled = !light.enabled;
+        if (IsHost || IsServer)
+        {
+            // If is host, then as mentioned above client rpc excludes server, so have to enforce that action locally separately, then invoke client rpc for network syncing
+            light.enabled = !light.enabled;
+            ToggleFlashLightClientRpc();
+        } else
+        {
+            ToggleFlashLightServerRpc();
+        }
         yield return null;
     }
 
